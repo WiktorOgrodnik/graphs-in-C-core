bba0ad7566d17f46961100e89fa896d56e7f6400
Wiktor Ogrodnik <wiktor2800-o@outlook.com>
2022-05-08 08:44:37 +0200

    fixes

--- main.c
+++ main.c
@@ -30,7 +30,7 @@ int main (int argc, char** argv) {
         else if (opt == 'i') interval = atoi(optarg);
         else if (opt == 'f') {
             fflag = true;
-            sprintf(fileName, "%s\n", optarg);
+            sprintf(fileName, "%s", optarg);
         }
         else if (opt == 'p') pflag = true;
         else if (opt == 'h') {
